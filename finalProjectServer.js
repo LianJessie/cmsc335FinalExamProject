ae979ab09f1926431e3d23df861f276174490323
LianJessie <115849879+LianJessie@users.noreply.github.com>
2024-12-15 23:28:03 -0500

    Add files via upload

--- finalProjectServer.js
+++ finalProjectServer.js
@@ -26,15 +26,123 @@ app.set("view engine", "ejs");
 
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 
-const portNumber = Number(process.argv[2]);
+const portNumber = 5000;
+
+async function getCoffee() {
+    const coffeeResponse = await fetch('https://coffee.alexflipnote.dev/random.json')
+    await coffeeResponse.json()
+        .then(data => {
+
+            return data;
+
+            console.log('Random Coffee Picture URL:', data.file);
+            // You can use the image URL to display the coffee picture on your website
+            const img = document.createElement('img');
+            img.src = data.file;
+            document.body.appendChild(img);
+
+
+        })
+        .catch(error => console.error('Error fetching coffee picture:', error));
+}
+
+
 
 app.get("/", (request, response) => {
+    //<img id = "coffee" src = "https://coffee.alexflipnote.dev/random">
+    apiData = getCoffee();
+    apiPicture = `<img id = "coffee" src = "${apiData.file}" alt = "Random Coffee Picture">`
+
+
+    variables = {coffeePicture: apiPicture}
+
+    response.render("home", variables);
+});
+
+app.get("/placeOrder", (request, response) => {
+    response.render("placeOrder");
+});
+
+app.post("/placeOrderConfirmation", async (request, response) => {
+    let {drink, name, phone, message} = request.body;
+    orderPlace(drink, name, phone, message);
     response.render("home");
 });
 
-app.listen(portNumber);
-console.log(`Web server started and running at http://localhost:${portNumber}`);
+app.get("/orderHistory", (request, response) => {
+    response.render("orderHistory");
+});
 
+app.post("/orderHistoryList", async (request, response) => {
+
+    let {orderName} = request.body;
+
+    historyList = "";
+
+    try {
+        await client.connect();
+        let filter = {name: orderName};
+        const cursor = await client.db(databaseAndCollection.db)
+                        .collection(databaseAndCollection.collection)
+                        .find(filter);
+    
+        const result = await cursor.toArray();
+
+        if ((result) && (result.length > 0)) {
+            result.forEach((element =>
+
+                historyList += 
+                `<div id = "entryList">
+                    <div class = "orderEntry">
+                        Drink: "${element.drink}"
+                        <br>
+                        Phone #: ${element.phone}
+                        <br>
+                        Date: ${element.date}
+                        <br>
+                        Message: "${element.message}"
+                    </div>
+                </div>`
+            ));
+        } else {
+            historyList += `<div id = "entryList"><div class = "orderEntry">NONE</div></div>`
+        }
+        
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+    variables = {
+        historyList: historyList
+    }
+
+    response.render("orderHistoryList", variables);
+});
+
+async function orderPlace(drink, name, phone, message) {
+    try {
+        await client.connect();
+        
+        let application = {
+            drink: drink,
+            name: name, 
+            phone: phone, 
+            date: new Date(), 
+            message: message
+        };
+
+        await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(application);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+app.listen(portNumber);
+/*
 const prompt = "Stop to shutdown the server: ";
 process.stdout.write(prompt);
 process.stdin.on("readable", function () {
@@ -55,4 +163,4 @@ process.stdin.on("readable", function () {
         process.stdout.write(prompt);
         process.stdin.resume();
     }
-});
+});*/
